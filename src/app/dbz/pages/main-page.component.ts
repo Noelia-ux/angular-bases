cc7ce9664aeaec74dd4f3258915fda1b73d540cf
Noelia-ux <noelia.cardosorubio@gmail.com>
2024-08-07 00:50:19 +0200

    actualizando servicios y MainPage

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Character } from './../interfaces/character.interface';
 import { Component} from '@angular/core';
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -9,27 +10,8 @@ import { Component} from '@angular/core';
 
 export class MainPageComponent {
 
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  },{
-    name:'Goku',
-    power: 9500
-  },{
-    name:"Vegeta",
-    power: 7500
-  }];
+constructor(public dbzService:DbzService){
 
- // onDelete = Index value: number;
-
-  onNewCharacter(character:Character): void{
-    this.characters.push(character);
-  }
-
-  onDeleteCharacter(index:number){
-
-    //this.characters.splice(1,2)
-    this.characters.splice(index,1);
-  }
+}
 
 }
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
+
 import { Character } from '../interfaces/character.interface';
 
+
 //Hacemos cambios para subirlo a git
-@Injectable({providedIn: 'root'})
+@Injectable({
+  providedIn: 'root'
+})
 export class DbzService {
 
   public characters: Character[] = [{
-    name: 'Krillinnn',
+    id: uuid(),
+    name: 'Krillj',
     power: 1000
   },{
+    id: uuid(),
     name:'Goku',
     power: 9500
   },{
+    id: uuid(),
     name:"Vegetaaa",
     power: 7500
   }];
